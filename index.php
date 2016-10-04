b3c8211b8e926c6b02230346ff366071a580a87b
Theo Guerin <guerin@info27-11.iut45.univ-orleans.fr>
2016-10-04 14:16:42 +0200

    réorganisation du projet

--- index.php
+++ index.php
@@ -3,16 +3,7 @@
 require_once __DIR__."/vendor/autoload.php";
 
 $app = new Silex\Application();
-$app["debug"] = true;
 
-$app["baseRoute"] = '';
-
-$app->get("/",function(){
-    return "toto";
-});
-
-$app->get("/hello/{name}",function($name){
-    return "salut $name";
-});
+require_once __DIR__."/app/app.php";
 
 $app->run();

ef35d275c8abf4e9d1952debb3bb5938a7df127a
Theo Guerin <guerin@info27-11.iut45.univ-orleans.fr>
2016-10-06 10:17:05 +0200

    ajout de l'app dans l'action

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -2,11 +2,13 @@
 
 namespace TheoGuerin\Controllers;
 
+use Silex\Application;
+
 class HomeController{
-    public function indexAction(){
+    public function indexAction(Application $app){
         return "ca marche";
     }
-    public function helloAction($name){
+    public function helloAction(Application $app, $name){
         return "Bonjour $name";
     }
 }

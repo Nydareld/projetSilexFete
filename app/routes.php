5cf0a79531d4f01cd3f49dd2890a94b0a4d11af7
Théo Guerin <theo.guerin@nr-communication.fr>
2016-10-12 22:17:58 +0200

    changement de la route de création de base

--- app/routes.php
+++ app/routes.php
@@ -3,6 +3,6 @@
 $app->get('/',"TheoGuerin\Controllers\HomeController::indexAction");
 $app->get('/hello/{name}',"TheoGuerin\Controllers\HomeController::helloAction");
 
-$app->get('/createBase',"TheoGuerin\Controllers\DataBaseController::createBaseAction");
+$app->get('/admin/createBase',"TheoGuerin\Controllers\DataBaseController::createBaseAction");
 
 $app->get('api/persones',"TheoGuerin\Controllers\API\PersonneController::getAllAction");

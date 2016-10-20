bf0b4c6b8b7078c086f14697586ca98a5bfc7b77
Théo Guerin <theo.guerin@nr-communication.fr>
2016-10-20 08:57:18 +0200

    route d'ajout de personne et son cntroller

--- app/routes.php
+++ app/routes.php
@@ -6,4 +6,5 @@ $app->get('/hello/{name}',"TheoGuerin\Controllers\HomeController::helloAction");
 $app->get('/admin/createBase',"TheoGuerin\Controllers\DataBaseController::createBaseAction");
 
 $app->get('api/persones',"TheoGuerin\Controllers\API\PersonneController::getAllAction");
+$app->post('api/persones',"TheoGuerin\Controllers\API\PersonneController::createAction");
 $app->get('api/persones/{id}',"TheoGuerin\Controllers\API\PersonneController::getOneByIdAction");
--- src/Controllers/API/PersonneController.php
+++ src/Controllers/API/PersonneController.php
@@ -3,6 +3,9 @@
 namespace TheoGuerin\Controllers\API;
 
 use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+
+use TheoGuerin\Models\Personne;
 
 class PersonneController{
 
@@ -13,6 +16,16 @@ class PersonneController{
 
     public function getOneByIdAction(Application $app,$id){
         $personne = $app["dao.Personne"]->getOneById($id);
+        return $app->json($personne);
+    }
+
+    public function createAction(Request $request, Application $app){
+        $personne = new Personne();
+        foreach (Personne::getFields() as $field) {
+            $function = "set".ucfirst($field);
+            $personne->$function($request->request->get($field));
+        }
+        $personne = $app["dao.Personne"]->saveObject($personne);
         return $app->json($personne);
     }
 

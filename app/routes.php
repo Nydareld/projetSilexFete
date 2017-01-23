2e4a88c9006ddf71446d39d68a5be9996acbb152
Théo Guerin <theo.guerin@nr-communication.fr>
2017-01-23 14:19:31 +0100

    routes pour produit

--- app/routes.php
+++ app/routes.php
@@ -1,1 +1,7 @@
 <?php
+// Product Get
+$app->get('/api/product',"TheoGuerin\ApiController\ProductController::getAllProductAction");
+$app->get('/api/product/{id}',"TheoGuerin\ApiController\ProductController::getProductAction");
+$app->post('/api/product',"TheoGuerin\ApiController\ProductController::postProductAction");
+$app->put('/api/product/{id}',"TheoGuerin\ApiController\ProductController::putProductAction");
+$app->delete('/api/product/{id}',"TheoGuerin\ApiController\ProductController::deleteProductAction");

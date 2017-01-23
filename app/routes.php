888d1e856ff32c1c8da8b6b51c228c7f89d86424
Clément <clement.lecomte6@orange.fr>
2017-01-23 15:45:39 +0100

    Ajout des routes pour l'evenement

--- app/routes.php
+++ app/routes.php
@@ -1,7 +1,14 @@
 <?php
-// Product Get
+// Product
 $app->get('/api/product',"TheoGuerin\ApiController\ProductController::getAllProductAction");
 $app->get('/api/product/{id}',"TheoGuerin\ApiController\ProductController::getProductAction");
 $app->post('/api/product',"TheoGuerin\ApiController\ProductController::postProductAction");
 $app->put('/api/product/{id}',"TheoGuerin\ApiController\ProductController::putProductAction");
 $app->delete('/api/product/{id}',"TheoGuerin\ApiController\ProductController::deleteProductAction");
+
+// Event
+$app->get('/api/product',"TheoGuerin\ApiController\EventController::getAllEventAction");
+$app->get('/api/product/{id}',"TheoGuerin\ApiController\EventController::getEventAction");
+$app->post('/api/product',"TheoGuerin\ApiController\EventController::postEventAction");
+$app->put('/api/product/{id}',"TheoGuerin\ApiController\EventController::putEventAction");
+$app->delete('/api/product/{id}',"TheoGuerin\ApiController\EventController::deleteEventAction");

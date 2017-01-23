6682fc20a0167bfc99700508c396f9d21324f007
Théo Guerin <theo.guerin@nr-communication.fr>
2017-01-23 14:18:43 +0100

    Gegister du service hydrateur

--- app/app.php
+++ app/app.php
@@ -1,15 +1,19 @@
 <?php
 
+use Symfony\Component\Debug\ErrorHandler;
+use Symfony\Component\Debug\ExceptionHandler;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 require_once __DIR__.'/config/config.php';
 
 $app["debug"] = true;
 
 
-$app->before(function(Request $request){
-    if(0 === strpos($request->headers->get('Content-Type'),'application/json')){
-        $data = json_decode($request->getContent(),true);
+// === Milldleware d'entrée ===
+$app->before(function (Request $request) {
+    if (0 === strpos($request->headers->get('Content-Type'), 'application/json')) {
+        $data = json_decode($request->getContent(), true);
         $request->request->replace(is_array($data) ? $data : array());
     }
 });
@@ -19,3 +23,12 @@ use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
 $config = Setup::createAnnotationMetadataConfiguration(array(__DIR__."/../src/Model"), $app['debug']);
 $app['orm.em'] = EntityManager::create($app['db.options'], $config);
+
+
+
+$app["hydrator"] = function () {
+    return new TheoGuerin\Service\Hydrator();
+};
+
+// === DAOS ===
+require_once __DIR__.'/daos.php';

f59766b4106757bc9629e7075d57b2e79c59980a
Clément <clement.lecomte6@orange.fr>
2017-01-23 16:21:06 +0100

    creation des routes pour event

--- app/routes.php
+++ app/routes.php
@@ -7,8 +7,8 @@ $app->put('/api/product/{id}',"TheoGuerin\ApiController\ProductController::putPr
 $app->delete('/api/product/{id}',"TheoGuerin\ApiController\ProductController::deleteProductAction");
 
 // Event
-$app->get('/api/product',"TheoGuerin\ApiController\EventController::getAllEventAction");
-$app->get('/api/product/{id}',"TheoGuerin\ApiController\EventController::getEventAction");
-$app->post('/api/product',"TheoGuerin\ApiController\EventController::postEventAction");
-$app->put('/api/product/{id}',"TheoGuerin\ApiController\EventController::putEventAction");
-$app->delete('/api/product/{id}',"TheoGuerin\ApiController\EventController::deleteEventAction");
+$app->get('/api/event',"TheoGuerin\ApiController\EventController::getAllEventAction");
+$app->get('/api/event/{id}',"TheoGuerin\ApiController\EventController::getEventAction");
+$app->post('/api/event',"TheoGuerin\ApiController\EventController::postEventAction");
+$app->put('/api/event/{id}',"TheoGuerin\ApiController\EventController::putEventAction");
+$app->delete('/api/event/{id}',"TheoGuerin\ApiController\EventController::deleteEventAction");

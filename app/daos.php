8fdc0be474aff89068d02a6c441d3933ee5372cb
Clément <clement.lecomte6@orange.fr>
2017-01-23 16:23:19 +0100

    ajout de la ligne dans le daos pour l'event

--- app/daos.php
+++ app/daos.php
@@ -4,3 +4,7 @@
 $app["dao.product"] = function ($app) {
     return new TheoGuerin\DAO\ProductDao($app['orm.em']);
 };
+
+$app["dao.event"] = function ($app) {
+    return new TheoGuerin\DAO\EventDao($app['orm.em']);
+};

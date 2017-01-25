c8d67805bb6057e73e751a088a149ad2fcf91db2
Théo Guerin <theo.guerin@nr-communication.fr>
2017-01-25 10:05:29 +0100

    Route options

--- app/routes.php
+++ app/routes.php
@@ -1,5 +1,10 @@
 <?php
 // Product Route
+
+$app->options("{anything}", function () {
+    return new \Symfony\Component\HttpFoundation\JsonResponse(null, 204);
+})->assert("anything", ".*");
+
 $app->get('/api/product',"TheoGuerin\ApiController\ProductController::getAllProductAction");
 $app->get('/api/product/{id}',"TheoGuerin\ApiController\ProductController::getProductAction");
 $app->post('/api/product',"TheoGuerin\ApiController\ProductController::postProductAction");

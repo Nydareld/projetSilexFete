ea547ccf2d5157d3734f5c65a95e315e115c9d7a
Théo Guerin <theo.guerin@nr-communication.fr>
2017-01-25 23:14:47 +0100

    fonction de champ a ignorer lors du serialize

--- src/Model/AbstractEntity.php
+++ src/Model/AbstractEntity.php
@@ -8,11 +8,15 @@ abstract class AbstractEntity implements JsonSerializable{
 
     public function jsonSerialize(){
         $res = array();
-        foreach ($this->getFields() as $field) {
+        foreach (array_diff($this->getFields(),$this->getIgnoredFields()) as $field) {
             $getter = "get".ucfirst( $field );
             $res[$field] = $this->$getter();
         }
         return $res;
+    }
+
+    public static function getIgnoredFields(){
+        return array();
     }
 
     abstract public static function getFields();

fc0030b566e04d91946dbc6b14cf4f0dacccdf00
Théo Guerin <theo.guerin@nr-communication.fr>
2017-01-25 10:08:22 +0100

    liens active du menu de gauche

--- backoffice/src/Blocks/SideBar/Controller.js
+++ backoffice/src/Blocks/SideBar/Controller.js
@@ -2,6 +2,4 @@ app.controller('Block.SideBar.Controller', ['$scope','$location',function($scope
     $scope.getClass = function (path) {
         return ($location.path().substr(0, path.length) === path) ? 'active' : '';
     }
-    console.log($location.path());
-    console.log("oé");
 }]);

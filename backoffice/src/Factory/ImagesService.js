8a36534ada0919b5c3753d0f5aea6ce80e96719a
Théo Guerin <theo.guerin@nr-communication.fr>
2017-01-30 04:37:55 +0100

    service pour le post d'images

--- backoffice/src/Factory/ImagesService.js
+++ backoffice/src/Factory/ImagesService.js
@@ -1,21 +1,30 @@
 app.factory('app.images', ['$http', function($http) {
 
-    var categories = 'http://fete.lc/api/image/categories';
-    var category = 'http://fete.lc/api/images/category';
+    var categoriesURL = 'http://fete.lc/api/images/categories';
+    var categoryURL = 'http://fete.lc/api/images/category';
+    var postImageURL = 'http://fete.lc/api/images';
 
     this.getCategoriesList = function(){
         return $http({
             method: 'GET',
-            url: categories
+            url: categoriesURL
         });
     };
 
     this.getCategory = function(catergoryName){
         return $http({
             method: 'GET',
-            url: category+'/'+catergoryName
+            url: categoryURL+'/'+catergoryName
         });
     };
+
+    this.save = function(image){
+        return $http({
+            method: 'POST',
+            url: postImageURL,
+            data : image
+        });
+    }
 
     return this;
 

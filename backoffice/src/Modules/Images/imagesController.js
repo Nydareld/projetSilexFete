f6ab54f3ae5f3dd184eea5bed04f53a65dec95b4
Théo Guerin <theo.guerin@nr-communication.fr>
2017-01-30 04:38:19 +0100

    modal et controller d'ajout d'images

--- backoffice/src/Modules/Images/imagesController.js
+++ backoffice/src/Modules/Images/imagesController.js
@@ -2,13 +2,6 @@ app.controller('imagesController', ['$scope','app.images','$filter',function($sc
 
     var me = this;
 
-    me.refreshCategory = function(){
-        service.getCategoriesList().then(function(res){
-            $scope.categories = res.data.data;
-            $scope.setCurrentCategory($scope.categories[0].name);
-        });
-    }
-
     $scope.properties=[
         {
             name : "Nom",
@@ -32,7 +25,33 @@ app.controller('imagesController', ['$scope','app.images','$filter',function($sc
                 return $filter('date')($filter('dateToISO')(value.date), 'dd/MM/yyyy HH:mm:ss',value.timezone);
             }
         }
-    ]
+    ];
+
+    me.refreshCategory = function(){
+        return service.getCategoriesList().then(function(res){
+            $scope.categories = res.data.data;
+            $scope.setCurrentCategory($scope.categories[0].name);
+        });
+    }
+    me.addImage = function(image){
+        me.refreshCategory().then(function(){
+            $scope.setCurrentCategory(image.category);
+        });
+    }
+
+    $scope.setNewImageCategory = function(category){
+        $scope.newImage.category = category;
+    }
+
+    $scope.clearNewImage = function(){
+        $scope.newImage = {
+            name : null,
+            description : null,
+            category : null,
+            path : null,
+            creator : null
+        }
+    }
     $scope.setCurrentCategory = function(categoryName){
         $scope.current = categoryName;
         if(!$scope.currentData){
@@ -55,6 +74,13 @@ app.controller('imagesController', ['$scope','app.images','$filter',function($sc
         }else {
             return "";
         }
+    }
+
+    $scope.addImage = function(newImage){
+        service.save(newImage).then(function(res){
+            me.addImage(res.data.data);
+            angular.element('#addImage').modal('hide');
+        });
     }
 
     me.refreshCategory();

c79cf2aa0cf5e2e631bc547883c30ae84cb5c5f0
Théo Guerin <theo.guerin@nr-communication.fr>
2017-02-01 02:30:08 +0100

    vue et controller de l'accueil

---
+++ frontoffice/src/Modules/Main/mainController.js
@@ -0,0 +1,10 @@
+app.controller('mainController', ['$scope','app.products',function($scope,service){
+    var me = this;
+    me.reloadProducts = function(){
+        service.cget().then(function(res){
+            $scope.products = res.data.data;
+        });
+    }
+    me.reloadProducts();
+
+}]);

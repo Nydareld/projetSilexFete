a85159ad804df513cb7ea7365acc0c59d1f21588
Théo Guerin <theo.guerin@nr-communication.fr>
2017-02-01 14:05:38 +0100

    hotfix active navbar

--- frontoffice/src/Blocks/NavBar/Controller.js
+++ frontoffice/src/Blocks/NavBar/Controller.js
@@ -1,5 +1,8 @@
 app.controller('Block.NavBar.Controller', ['$scope','$location',function($scope,$location){
     $scope.getClass = function (path) {
+        if(path == "home"){
+            return ($location.path() === "/") ? 'active' : '';
+        }
         return ($location.path().substr(0, path.length) === path) ? 'active' : '';
     }
 }]);

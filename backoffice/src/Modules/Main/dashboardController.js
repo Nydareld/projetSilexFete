db79af9806b131002e24a73f4c39c8546287475b
Théo Guerin <theo.guerin@nr-communication.fr>
2017-02-02 11:48:32 +0100

    contact et propositions dans le bo

--- backoffice/src/Modules/Main/dashboardController.js
+++ backoffice/src/Modules/Main/dashboardController.js
@@ -19,7 +19,19 @@ app.controller('dashboardController', ['$scope','$http','$filter','app.config',f
     }
 
     me.reloadContact = function(){
+        $http({
+            method: 'GET',
+            url: config.apiurl+"/api/contact"
+        }).then(function(res){
+            $scope.contacts = res.data.data;
+        });
+    }
 
+    $scope.delContact = function(id){
+        $http({
+            method: 'DELETE',
+            url: config.apiurl+"/api/contact/"+id
+        });
     }
 
     me.reloadProps();
